6978214d0090807bf7bac3fbb251e7d3613d1c39
soledaco <soledaco@oa-a2.msk.21-school.ru>
2025-08-05 11:10:45 +0300

    paths0.2

--- app/configs/vm_config.py
+++ app/configs/vm_config.py
@@ -1,10 +1,10 @@
 # VM_share/app/configs/vm_config.py
 from pathlib import Path
 
-VM_SHARE_ROOT = Path(__file__).resolve().parents[0]  
+# VM_SHARE_ROOT = Path(__file__).resolve().parents[0]  
 ALPINE_IMAGE_NAME = 'alpine.qcow2'
-ALPINE_IMAGE_PATH = str(VM_SHARE_ROOT / "base_images" / "Alpine_Linux")
-ALPINE_OVERLAYS_DIR = str(VM_SHARE_ROOT / "overlays" / "Alpine_Linux")
+ALPINE_IMAGE_PATH = str("/root" / "base_images" / "Alpine_Linux")
+ALPINE_OVERLAYS_DIR = str("/root"  / "overlays" / "Alpine_Linux")
 ALPINE_MEMORY = 512
 
 NOVNC_PROXY = str(Path.home() / "noVNC/utils/novnc_proxy")
