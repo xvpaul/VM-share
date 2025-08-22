34f468b50039cf8af60bcb5e426bab1d46dc0837
soledaco <soledaco@oa-a2.msk.21-school.ru>
2025-08-22 11:34:14 +0300

    VPS/addition

--- app/static/scripts/signup.js
+++ app/static/scripts/signup.js
@@ -63,8 +63,7 @@ document
         body: JSON.stringify({ login, password }),
       });
       const data = await res.json();
-      alert(data) // to remove
-      if (!res.ok) throw new Error(data.detail || "Registration failed");
+alert(JSON.stringify(data, null, 2)); // formatted JSON      if (!res.ok) throw new Error(data.detail || "Registration failed");
       console.log("Signed up:", data);
       window.location.href = "/";
     } catch (err) {
