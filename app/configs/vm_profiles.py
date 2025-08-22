da9b793f25885603eb6849ac1b189179dc3e4cf9
soledaco <soledaco@oa-a2.msk.21-school.ru>
2025-08-22 19:07:16 +0300

    VPS/addition

--- app/configs/vm_profiles.py
+++ app/configs/vm_profiles.py
@@ -27,6 +27,7 @@ VM_PROFILES = {
         "default_memory": 2048,
     },
     "custom": {
-        "base_image": Path("/root/myapp/custom/{uid}.iso"),
+        "base_image": Path("/root/myapp/custom/")
+        # "base_image": Path("/root/myapp/custom/{uid}.iso"), <---- prev
     }
 }
