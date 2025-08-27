3ff3b9605a729bd6f2fef02b26cbe225136ee30a
soledaco <soledaco@oa-a2.msk.21-school.ru>
2025-08-28 00:39:30 +0300

    VPS/addition

--- app/static/scripts/profile.js
+++ app/static/scripts/profile.js
@@ -438,7 +438,7 @@ async function runSnapshot(s, btn) {
 
     // Your /vm/run_snaphot currently expects { os_type, snapshot }.
     // (If you later add vmid in the API model, you can include it here too.)
-    const body = { os_type: osType, snapshot: baseName , vmid: vmid};
+    const body = { os_type: osType, userId: userId , vmid: vmid};
 
     const res = await fetch(RUN_VM_ENDPOINT, {
       method: 'POST',
