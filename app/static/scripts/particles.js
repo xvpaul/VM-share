c67aee475edd6fd2994025a50cce9f09d6eddb3f
soledaco <soledaco@oa-a2.msk.21-school.ru>
2025-09-02 05:44:10 +0300

    VPS/addition

--- app/static/scripts/particles.js
+++ app/static/scripts/particles.js
@@ -9,7 +9,7 @@
 particlesJS("particles-js", {
     particles: {
         number: {
-            value: 5,
+            value: 30,
             density: {
                 enable: true,
                 value_area: 800
